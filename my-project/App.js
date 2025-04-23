be9ceb8195729e8254a5b783055336bbe3787682
nmartinez71 <nmartinez71@ivytech.edu>
2025-04-23 00:13:16 -0400

    Structure, Navigation and some UI update

--- my-project/App.js
+++ my-project/App.js
@@ -1,20 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* 
+4/22/2025
 
+Set up some structural parts of the application. Navigation, some UI elements.
+
+The screens folder on the project directory is where the other screens (like Actors and Directors for example) will go when created.
+
+The img folder is where static images can be added.
+
+-Neftali
+*/
+
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View, Image, ImageBackground } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Movies from './screens/Movies';
+
+const Stack = createStackNavigator();
+
+const headerText = "MOVIES DATABASE"
+
+//Nav component
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="The Movie App" component={Home} />
+        <Stack.Screen name="Movies" component={Movies} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function Home ({navigation}) {
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground style={styles.bgImage} source={require('./img/movieBGImage.jpg')}>
+        <View style={styles.padContent}>
+          <Text style={styles.topHeaderText}>{headerText.split(' ').join('\n')}</Text> {/* The split and join code makes a new line for every word in the string */}
+        </View>
+      </ImageBackground>
+  
+
+      <View style={styles.content}>
+        <View style={styles.topBox}>
+          <TouchableOpacity style={styles.padContent} onPress={() => navigation.navigate('Movies')}>
+            <Text style={{ fontSize: 20 }}>🎬 MOVIES</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.padContent} onPress={() => navigation.navigate('Movies')}>
+            <Text style={{ fontSize: 20 }}>🎬 ACTORS</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.padContent} onPress={() => navigation.navigate('Movies')}>
+            <Text style={{ fontSize: 20 }}>🎬 DIRECTORS</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  bgImage: {
+    width: '100%',
+    height: 200,
+    justifyContent: 'flex-end',
+  },
+  topHeaderText: {
+    color: '#fff',
+    fontSize: 40,
+  },
+  bottomHeaderText: {
+    color: '#fff',
+    fontSize: 46,
+  },
+  content: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'flex-start',
+  },
+  topBox: {
+    alignItems: 'flex-start',
+  },
+  padContent: {
+    padding: 10
+  }
 });
---
+++ my-project/screens/Movies.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, StatusBar } from "react-native";
+
+
+export default function Movies() {
+  return (
+    <View>
+      <StatusBar hidden={false} />
+    </View>
+  );
+}
